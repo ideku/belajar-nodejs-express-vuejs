905a5f16f0d66449b1341c1114cbf487b2c3f026
Aulia Abrar <a6rar@yahoo.com>
2022-06-09 10:45:33 +0700

    Membuat skrip program pada file main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,43 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import vueConfig from 'vue.config'
+
+import App from './App.vue'
+import Create from './components/AddProduct.vue'
+import Edit from './components/EditProduct.vue'
+import Index from './components/ProductList.vue'
+
+Vue.use(VueRouter)
+
+Vue.config.productionTip = false
+
+const routes = [
+    {
+        name: 'Create',
+        path: '/create',
+        component: Create
+    },
+    {
+        name: 'Edit',
+        path: '/edit/:id',
+        component: Edit
+    },
+    {
+        name: 'Index',
+        path: '/',
+        component: Index
+    },
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes })
+
+new Vue({
+    // init router
+    router,
+    render: h => h(App),
+}).$mount('#app')
